1743bcc197c1c90e9a0fd045ef3c2fbd9a81bd45
CYT <cyt110427@naver.com>
2025-07-03 15:43:29 +0900

    onClick 사용법 익히기

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import './App.css'
 
 function App() {
   let post = '강남 우동 맛집';
-  let [글제목,A] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [name,setName] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [good,setGood] = useState(0);
 
 
   return (
@@ -15,15 +16,19 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
       <div className='list'>
-        <h4>{글제목[0]}</h4>
+        <h4>{name[0]} <span onClick={ ()=> { setGood(good+1)}}>👍</span> {good} </h4>
         <p>2월 17일 발행</p>
+        <p><span onClick={ () => {
+          const newName = [...name];
+          newName[0] ='여자 코트 추천';
+          setName(newName)}}>글 제목 변경</span></p>
       </div>
       <div className='list'>
-        <h4>{글제목[1]}</h4>
+        <h4>{name[1]}</h4>
         <p>2월 17일 발행</p>
       </div>
           <div className='list'>
-        <h4>{글제목[2]}</h4>
+        <h4>{name[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
     </div>
