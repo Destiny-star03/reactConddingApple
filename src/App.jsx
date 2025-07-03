6aad3209e6202993cf9784cfd4b5949d97c4d39a
CYT <cyt110427@naver.com>
2025-07-03 15:01:44 +0900

    state 학습

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/*eslint-disable*/
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -5,17 +6,27 @@ import './App.css'
 
 function App() {
   let post = '강남 우동 맛집';
+  let [글제목,A] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+
 
   return (
-    <>
-       <div className='black-nav'>
-        <h4 style={{color:'red', fontsize : '16px'}}>블로그임</h4>
-       </div>
-       <div className='list'>
-
-       </div>
-      <h4>{ post }</h4>
-    </>
+    <div className="App">
+      <div className='black-nav'>
+        <h4>ReactBlog</h4>
+      </div>
+      <div className='list'>
+        <h4>{글제목[0]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+          <div className='list'>
+        <h4>{글제목[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+    </div>
   )
 }
 
