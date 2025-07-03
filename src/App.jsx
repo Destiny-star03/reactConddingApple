d959a95e32b78a8b353cd9f89e5655650adf8943
CYT <cyt110427@naver.com>
2025-07-03 16:19:24 +0900

    array/object 일때 state 동작 원리 학습하기

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,40 @@ import './App.css'
 
 function App() {
   let post = '강남 우동 맛집';
-  let [name,setName] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
-  let [good,setGood] = useState(0);
+  let [name, setName] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [good, setGood] = useState(0);
+
+
 
 
   return (
     <div className="App">
+      
       <div className='black-nav'>
         <h4>ReactBlog</h4>
       </div>
+
+      <button onClick={() => {name.sort(); setName([...name])}}>
+        가나다순 정렬
+      </button>
+
       <div className='list'>
-        <h4>{name[0]} <span onClick={ ()=> { setGood(good+1)}}>👍</span> {good} </h4>
+        <h4>{name[0]} <span onClick={() => { setGood(good + 1) }}>👍</span> {good} </h4>
         <p>2월 17일 발행</p>
-        <p><span onClick={ () => {
-          const newName = [...name];
-          newName[0] ='여자 코트 추천';
-          setName(newName)}}>글 제목 변경</span></p>
       </div>
       <div className='list'>
         <h4>{name[1]}</h4>
         <p>2월 17일 발행</p>
       </div>
-          <div className='list'>
+      <div className='list'>
         <h4>{name[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
+      <button onClick={() => {
+       let copy = [...name];
+       copy[0] = '여자 코트 추천';
+       setName(copy);
+      }}>글수정</button>
     </div>
   )
 }
