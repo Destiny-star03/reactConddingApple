fd602438a3fb9852a8f15a9374fd93e8bf3ce806
CYT <cyt110427@naver.com>
2025-07-03 16:40:39 +0900

    컴포넌트 활용법 학습

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,9 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
 
-      <button onClick={() => {name.sort(); setName([...name])}}>
+      {/* <button onClick={() => {name.sort(); setName([...name])}}>
         가나다순 정렬
-      </button>
+      </button> */}
 
       <div className='list'>
         <h4>{name[0]} <span onClick={() => { setGood(good + 1) }}>👍</span> {good} </h4>
@@ -35,12 +35,28 @@ function App() {
         <h4>{name[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
-      <button onClick={() => {
+
+        <Modal></Modal>
+  
+      
+
+
+      {/* <button onClick={() => {
        let copy = [...name];
        copy[0] = '여자 코트 추천';
        setName(copy);
-      }}>글수정</button>
+      }}>글수정</button> */}
     </div>
+  )
+}
+
+function Modal(){
+  return (
+    <div className="modal">
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
   )
 }
 
